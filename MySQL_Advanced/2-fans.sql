91d26d257e15ee25245009f531f59ac5c5597876
vagifrasulzade <vaqufresulzade@gmail.com>
2026-03-19 16:29:42 +0400

    sql

---
+++ MySQL_Advanced/2-fans.sql
@@ -0,0 +1,6 @@
+-- Create a table named `metal_bands` with the following columns:
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY
+    origin
+ORDER BY nb_fans DESC;
---
+++ MySQL_Advanced/metal_bands.sql
@@ -0,0 +1,22 @@
+CREATE TABLE metal_bands (
+    band_name VARCHAR(255),
+    origin VARCHAR(255),
+    formed INT,
+    split INT,
+    style VARCHAR(255),
+    fans INT
+);
+
+INSERT INTO metal_bands VALUES
+('Alice Cooper', 'USA', 1968, 2024, 'Glam rock', 40000),
+('Motley Crue', 'USA', 1981, 2015, 'Glam rock', 35000),
+('Marilyn Manson', 'USA', 1989, 2020, 'Glam rock', 30000),
+('The 69 Eyes', 'Finland', 1990, 2020, 'Glam rock', 25000),
+('Hardcore Superstar', 'Sweden', 1997, 2020, 'Glam rock', 20000),
+('Nasty Idols', 'Sweden', 1987, 1987, 'Glam rock', 15000),
+('Hanoi Rocks', 'Finland', 1980, 1980, 'Glam rock', 10000),
+('Metallica', 'USA', 1981, 2024, 'Heavy Metal', 50000),
+('Slayer', 'USA', 1981, 2019, 'Thrash Metal', 49349),
+('In Flames', 'Sweden', 1990, 2024, 'Melodic Death Metal', 47619),
+('Nightwish', 'Finland', 1996, 2024, 'Symphonic Metal', 32878),
+('Iron Maiden', 'United Kingdom', 1975, 2024, 'Heavy Metal', 35679);
